69d81e41e570b664492262d0f9a6d3b9e8a6bac6
vinoarystio <rocevo.id@gmail.com>
2022-08-09 23:24:30 +0700

    init

--- src/Components/ModalDelete.jsx
+++ src/Components/ModalDelete.jsx
@@ -42,7 +42,7 @@ const ModalDelete = ({
           className={cssModule.Components.modal}
           onClick={closeModal}
           ref={modalRef}
-          data-cy="activity-item-delete-button"
+          data-cy="modal-delete"
         >
           <div className={cssModule.Components.modalDelete}>
             <div>
